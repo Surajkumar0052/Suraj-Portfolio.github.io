f45311e77a655a65cd630108229a69882b337173
Surajkumar0052 <83065955+Surajkumar0052@users.noreply.github.com>
2021-10-28 20:22:01 +0530

    My Portfolio

---
+++ index.js
@@ -0,0 +1,15 @@
+let contact = document.getElementById('contact');
+
+contact.addEventListener('click', contact1);
+
+function contact1(){
+    window.location.href = "#contact-us";
+}
+
+let resume = document.getElementById('resume-new');
+resume.addEventListener('click', viewResume);
+
+function viewResume(){
+    window.location.href = "https://drive.google.com/file/d/1aW7eKFLfD5-CgKNpPepZIayN8zTxkluX/view?usp=sharing";
+    
+}
